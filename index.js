56123d7f6ed482dda429e7e83da52e7d55768694
Jan De Wilde <jan@jandewilde.com>
2022-04-16 21:49:21 -0400

    Working version

--- index.js
+++ index.js
@@ -4,8 +4,7 @@
 
 const grid = document.querySelector('#grid');
 const gridSize = 4;
-const numberOfTiles = gridSize ** 2; //?
-
+const numberOfTiles = gridSize ** 2;
 const emojis = [
   '💩',
   '🥳',
@@ -35,18 +34,17 @@ const emojis = [
   '🇺🇸',
 ];
 
-let numberOfClicks = 0; 
+let secondClick = false;
 let shuffledEmojis;
 let uniqueBoardEmojis;
 let allBoardEmojis;
-
-let firstTileId;
-// let secondTileId;
+let firstTile;
 
 /**
  * Shuffles array in place. ES6 version
  * @param {Array} a items An array containing the items.
  */
+
 function shuffleArray(a) {
   for (let i = a.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
@@ -73,57 +71,57 @@ const generateGridInDOM = (grid, gridSize, emojis) => {
   }
   // create tiles HTML
   for (let i = 0; i < gridSize ** 2; i++) {
-    gridHTML += `<button><div id="tile--${i}">${emojis[i]}</div></button>`;
+    gridHTML += `<button type="button" id="tile--${i}" class="btn"><div>${emojis[i]}</div></button>`;
   }
   // Insert in DOM
   grid.insertAdjacentHTML('beforeend', gridHTML);
 };
 
+function handleClick(e) {
+  let button;
+
+  if (e.target.matches('div')) {
+    button = e.target.parentElement;
+  } else {
+    button = e.target;
+  }
+
+  // Already visible? 
+  if (button.firstChild.classList.contains('visible')) {
+    return;
+  }
+  // First tile?
+  if (!secondClick) {
+    button.firstChild.classList.add('visible');
+    secondClick = !secondClick;
+    firstTile = button;
+    return;
+  }
+  // First tile visible, checking 2nd tile
+  if (secondClick) {
+    button.firstChild.classList.add('visible');
+    secondClick = !secondClick;
+    // if emojis are not the same, hide them
+    if (firstTile.firstChild.textContent !== button.firstChild.textContent) {
+      setTimeout(function () {
+        firstTile.firstChild.classList.remove('visible');
+        button.firstChild.classList.remove('visible');
+      }, 1000);
+    }
+  }
+}
+
+// MAIN
+
 // Shuffle the emoji's
 shuffledEmojis = shuffleArray(emojis);
-
 // Select the half of the gridsize emoji's
-// e.g. 6x6 = 36, so select 18 emojis
-
 uniqueBoardEmojis = shuffledEmojis.slice(0, numberOfTiles / 2);
-
-// Duplicate each emoji in the array
+// Now duplicate them and shuffle again
 allBoardEmojis = shuffleArray(duplicateArrayElements(uniqueBoardEmojis));
-// Shuffle the array
 
 setRootProperty('--grid-size', gridSize);
 
 generateGridInDOM(grid, gridSize, allBoardEmojis);
 
-
-
-
-grid.addEventListener('click', (e) => {
-  // Only if tile is not already visible
-  if (!e.target.classList.contains('visible')) {
-    // Are we revealing the first tile to get checked?
-    if (numberOfClicks === 0) {
-      // @TODO maybe I can store the e.target in variable instead of retrieving ID
-      // so I don't have to use .querySelector later?
-      firstTileId = e.target.getAttribute('id');
-      e.target.classList.add('visible');
-      numberOfClicks = 1;
-      return;
-    }
-    // Another tile is visible, waiting to get checked
-    if (numberOfClicks === 1) {
-      // secondTileId = e.target.getAttribute('id');
-      // Reveal the second tile
-      e.target.classList.add('visible');
-      let firstTile = document.querySelector(`#${firstTileId}`);
-        // if emojis are not the same, hide them
-        if (firstTile.textContent !== e.target.textContent) {
-          setTimeout(function(){
-            firstTile.classList.remove('visible');
-            e.target.classList.remove('visible');
-          }, 1000)
-        }
-      numberOfClicks = 0;
-    }
-  }
-});
+grid.addEventListener('click', handleClick);
